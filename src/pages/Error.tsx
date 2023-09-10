2c57da9ae4034bb9fb64909544ebb922f13b897a
jo-duchan <jo_duchan@icloud.com>
2023-09-11 00:04:26 +0900

    error page 임시 작업

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -2,10 +2,26 @@ import React from "react";
 import { useRouteError } from "react-router-dom";
 
 function ErrorPage() {
-  const error = useRouteError();
-  console.log(error);
+  const error = useRouteError() as any;
 
-  return <h1>ErrorPage </h1>;
+  let title = "An error occurred!";
+  let message = "Something went wrong!";
+
+  if (error.status === 500) {
+    message = error.data.message;
+  }
+
+  if (error.status === 404) {
+    title = "Not found!";
+    message = "Could not find resource or page.";
+  }
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{message}</p>
+    </>
+  );
 }
 
 export default ErrorPage;
