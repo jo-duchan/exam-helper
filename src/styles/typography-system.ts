26e9edcb73fb0674a4b4ab10eb4d593b5c24c960
jo-duchan <jo_duchan@icloud.com>
2023-09-25 00:32:46 +0900

    typography 시스템 수정 반영

--- src/styles/typography-system.ts
+++ src/styles/typography-system.ts
@@ -4,12 +4,12 @@ const Heading = {
   H1: css`
     font-size: 32px;
     font-weight: 700;
-    line-height: 34px;
+    line-height: 1.25em;
   `,
   H2: css`
     font-size: 26px;
     font-weight: 700;
-    line-height: 34px;
+    line-height: 1.25em;
   `,
 };
 
@@ -18,56 +18,56 @@ const Body = {
     XL: css`
       font-size: 18px;
       font-weight: 700;
-      line-height: normal;
+      line-height: 1.25em;
     `,
     L: css`
       font-size: 16px;
       font-weight: 700;
-      line-height: 24px;
+      line-height: 1.25em;
     `,
     M: css`
       font-size: 14px;
       font-weight: 700;
-      line-height: normal;
+      line-height: 1.25em;
     `,
   },
   SemiBold: {
     L: css`
       font-size: 16px;
       font-weight: 600;
-      line-height: 24px;
+      line-height: 1.25em;
     `,
     M: css`
       font-size: 14px;
       font-weight: 600;
-      line-height: normal;
+      line-height: 1.25em;
     `,
     S: css`
       font-size: 12px;
       font-weight: 600;
-      line-height: normal;
+      line-height: 1.25em;
     `,
     XS: css`
       font-size: 10px;
       font-weight: 600;
-      line-height: normal;
+      line-height: 1.25em;
     `,
   },
   Medium: {
     L: css`
       font-size: 16px;
       font-weight: 500;
-      line-height: normal;
+      line-height: 1.25em;
     `,
     M: css`
       font-size: 14px;
       font-weight: 500;
-      line-height: normal;
+      line-height: 1.25em;
     `,
     S: css`
       font-size: 12px;
       font-weight: 500;
-      line-height: normal;
+      line-height: 1.25em;
     `,
   },
 };
