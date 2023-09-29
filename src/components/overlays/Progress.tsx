1679d3a441c38af2d9bc9e8a1df4d8fe5464c850
jo-duchan <jo_duchan@icloud.com>
2023-09-29 12:13:00 +0900

    Overlay - Component - Progress 작업

--- src/components/overlays/Progress.tsx
+++ src/components/overlays/Progress.tsx
@@ -1,21 +1,37 @@
-import React from "react";
-import { CSSTransition } from "react-transition-group";
-import styled, { css, keyframes } from "styled-components";
-import { ReactComponent as Loading } from "assets/icon/Loading.svg";
+import styled, { keyframes } from "styled-components";
+import Loading from "assets/icon/loading.png";
 import ZIndex from "styles/z-index";
 
 function Progress() {
   return (
-    <CSSTransition in timeout={300}>
-      <Container>d</Container>
-    </CSSTransition>
+    <Container>
+      <img src={Loading} />
+    </Container>
   );
 }
 
 export default Progress;
 
+const LoadingKeyframes = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
 const Container = styled.div`
   width: 100%;
   height: 100%;
   ${ZIndex.MAX};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  & img {
+    width: 46px;
+    height: 46px;
+    animation: ${LoadingKeyframes} 1400ms ease-in-out infinite;
+  }
 `;
