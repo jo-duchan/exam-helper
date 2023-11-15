4183538147fc4ec30ea089873c15adfb80a2a65b
jo-duchan <jo_duchan@icloud.com>
2023-11-15 13:17:45 +0900

    feat: Redux Toolkit - Store 생성

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "App";
+import { Provider } from "react-redux";
+import store from "store/store";
 import OverlayContext from "contexts/OverlayContext";
 import { GlobalStyle } from "styles/common";
 
@@ -8,8 +9,10 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <OverlayContext>
-    <GlobalStyle />
-    <App />
-  </OverlayContext>
+  <Provider store={store}>
+    <OverlayContext>
+      <GlobalStyle />
+      <App />
+    </OverlayContext>
+  </Provider>
 );
