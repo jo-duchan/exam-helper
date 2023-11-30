c6fe5e3bf33f65694e31aaa04b7d4b8e24d324cb
jo-duchan <jo_duchan@icloud.com>
2023-12-01 01:09:05 +0900

    feat: MainPage Auth 연동 완료

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
-import { auth } from "firebase-config";
-import { useNavigate } from "react-router-dom";
+import type { UserInfo } from "store//auth.slice";
+import store from "store/store";
+import { useLoaderData, redirect } from "react-router-dom";
 import styled from "styled-components";
 import ZIndex from "styles/z-index";
 import { Heading } from "styles/typography-system";
@@ -14,44 +15,29 @@ import Actions from "components/main/Actions";
 import Information from "components/main/Information";
 import Fire from "assets/img/fire.png";
 
+interface LoaderData {
+  user: UserInfo | null;
+  userData: UserData;
+  bannerData: BannerData;
+}
+
 function MainPage() {
-  const navigate = useNavigate();
-  const [userData, setUserData] = useState({} as UserData);
-  const [bannerData, setBannerData] = useState({} as BannerData);
-  const [userName, setUserName] = useState<string>("사용자");
+  const { user, userData, bannerData } = useLoaderData() as LoaderData;
+  const [userName, setUserName] = useState("사용자");
   const [infoType, setInfoType] = useState<string>("signin");
   const [unsigned, setUnsigned] = useState<boolean>(true);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const onboarding = localStorage.getItem("onboarding");
-
-      if (onboarding !== "hide") {
-        navigate("/onboarding");
-      }
-
-      const bannerData: BannerData = await service.GET(`admin/banner`);
-      setBannerData(bannerData);
-
-      auth.onAuthStateChanged(async (result) => {
-        if (result) {
-          const userData: UserData = await service.GET(`users/${result.uid}/`);
-          setUserData(userData);
-          setUserName(result.displayName as string);
-          setInfoType("stats");
-          setUnsigned(false);
-        } else {
-          const playList: Play[] = await service.GET(`admin/playList`);
-          const sheetUrl: string = await service.GET(`admin/sheetUrl`);
-          setUserData((prev) => {
-            return { ...prev, playList, sheetUrl };
-          });
-        }
-      });
-    };
-
-    fetchData();
-  }, []);
+    if (user) {
+      setUserName(user.displayName as string);
+      setInfoType("stats");
+      setUnsigned(false);
+    } else {
+      setUserName("사용자");
+      setInfoType("signin");
+      setUnsigned(true);
+    }
+  }, [user]);
 
   return (
     <Container>
@@ -72,6 +58,29 @@ function MainPage() {
 }
 
 export default MainPage;
+
+export async function loader() {
+  const onboarding = localStorage.getItem("onboarding");
+
+  if (onboarding !== "hide") {
+    return redirect("/onboarding");
+  }
+
+  let userData: UserData;
+  const bannerData: BannerData = await service.GET(`admin/banner`);
+  const user = store.getState().auth.user;
+
+  if (user) {
+    userData = await service.GET(`users/${user.uid}/`);
+  } else {
+    const playList: Play[] = await service.GET(`admin/playList`);
+    const sheetUrl: string = await service.GET(`admin/sheetUrl`);
+
+    userData = { playList, sheetUrl } as UserData;
+  }
+
+  return { user, userData, bannerData };
+}
 
 const Container = styled.div`
   position: relative;
