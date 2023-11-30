00e76dce37debd4604755dd0e2ccfb12559b2a03
jo-duchan <jo_duchan@icloud.com>
2023-12-01 00:54:39 +0900

    fix: Auth 관련 개선

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,14 +1,33 @@
-import React, { useEffect } from "react";
-import { ScrollRestoration, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { ScrollRestoration, Outlet, useNavigate } from "react-router-dom";
 import store from "store/store";
 import { setUser, removeUser } from "store/auth.slice";
 import { auth } from "firebase-config";
 import { onAuthStateChanged } from "firebase/auth";
+import { showToast } from "utils/overlays";
 import inAppBypassing from "utils/inAppBypassing";
 
 function Layout() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     inAppBypassing();
+    onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        store.dispatch(
+          setUser({
+            uid: currentUser.uid,
+            displayName: currentUser.displayName!,
+            email: currentUser.email!,
+            createdAt: currentUser.metadata.creationTime!,
+          })
+        );
+      } else {
+        store.dispatch(removeUser());
+        showToast("다음에 또 만나요.", "notify");
+        navigate("/");
+      }
+    });
   }, []);
 
   return (
@@ -22,20 +41,18 @@ function Layout() {
 export default Layout;
 
 export async function loader() {
-  const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-    if (currentUser) {
-      store.dispatch(
-        setUser({
-          uid: currentUser.uid,
-          displayName: currentUser.displayName!,
-          email: currentUser.email!,
-          createdAt: currentUser.metadata.creationTime!,
-        })
-      );
-    } else {
-      store.dispatch(removeUser());
-    }
-  });
+  const user = auth.currentUser;
+
+  if (user) {
+    store.dispatch(
+      setUser({
+        uid: user.uid,
+        displayName: user.displayName!,
+        email: user.email!,
+        createdAt: user.metadata.creationTime!,
+      })
+    );
+  }
 
   return null;
 }
