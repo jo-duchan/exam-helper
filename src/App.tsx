3c603e7cdeac82e1484a8bc00f1b675877dcc3bb
jo-duchan <jo_duchan@icloud.com>
2023-12-03 01:34:02 +0900

    chore: index에 코드 app으로 옮겨서 정리

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { Provider } from "react-redux";
+import store from "store/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import RouterObject from "Routers";
+import { GlobalStyle } from "styles/common";
+import RenderOverlays from "components/overlays/RenderOverlays";
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <RouterProvider router={RouterObject} />;
+  return (
+    <Provider store={store}>
+      <GlobalStyle />
+      <RenderOverlays />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={RouterObject} />
+      </QueryClientProvider>
+    </Provider>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "App";
-import { Provider } from "react-redux";
-import store from "store/store";
-import { GlobalStyle } from "styles/common";
-import RenderOverlays from "components/overlays/RenderOverlays";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <Provider store={store}>
-    <GlobalStyle />
-    <RenderOverlays />
-    <App />
-  </Provider>
-);
+root.render(<App />);
