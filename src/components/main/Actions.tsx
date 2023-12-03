c737b007e5539f3cc22681fd82eae94f4101cb8c
jo-duchan <jo_duchan@icloud.com>
2023-12-03 21:55:07 +0900

    feat: Share 기능 추가, 코드 리펙토링

--- src/components/main/Actions.tsx
+++ src/components/main/Actions.tsx
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import styled, { css } from "styled-components";
 import Utils from "utils/utils";
+import { showProgress, hideProgress, showToast } from "utils/overlays";
 import { Heading, Body } from "styles/typography-system";
 import Color from "styles/color-system";
 import { UserData, ScoreList } from "types/user-data";
 import { ReactComponent as Arrow } from "assets/icon/arrow.svg";
+import { ReactComponent as Share } from "assets/icon/share.svg";
 import AddSheetImg from "assets/img/empty.png";
 
 interface Props {
@@ -19,32 +21,13 @@ function Actions({ data, unsigned }: Props) {
     scoreList = {} as ScoreList,
     totalStage = 0,
   } = data;
-  const getSheetName = (sheetName: string) => {
-    const data = Object.keys(scoreList)
+  const navigate = useNavigate();
+
+  const renderScore = (sheetName: string) => {
+    const getName = Object.keys(scoreList)
       .reverse()
       .find((item) => scoreList[item].sheetName === sheetName);
 
-    return data!;
-  };
-
-  const getSheetId = (sheetId: string | undefined) => {
-    if (!sheetId) {
-      return Utils.convertSheetUrl(sheetUrl);
-    }
-    return sheetId;
-  };
-
-  const renderDate = (key: string) => {
-    const getName = getSheetName(key);
-    if (getName) {
-      return Utils.dateFormat(scoreList[getName].date);
-    }
-
-    return "00.00.00";
-  };
-
-  const renderScore = (key: string) => {
-    const getName = getSheetName(key);
     if (getName) {
       return (
         <>
@@ -57,22 +40,52 @@ function Actions({ data, unsigned }: Props) {
     return <span className="score">000</span>;
   };
 
+  const goToQuizPage = (sheetId: string | undefined, sheetName: string) => {
+    const genSheetId = !sheetId ? Utils.convertSheetUrl(sheetUrl) : sheetId;
+
+    navigate(`/quiz?id=${genSheetId}&name=${sheetName}&stage=${totalStage}`);
+  };
+
+  const shareItem = async (sheetId: string | undefined, sheetName: string) => {
+    const genSheetId = !sheetId ? Utils.convertSheetUrl(sheetUrl) : sheetId;
+    const shareUrl = `${window.location.href}download?id=${genSheetId}&name=${sheetName}`;
+
+    try {
+      showProgress();
+      if (navigator.share ?? false) {
+        await navigator.share({
+          title: "이그잼 헬퍼와 함께 성장해요!",
+          text: `링크를 클릭하시면 문제(${sheetName})를 공유 받을 수 있어요.`,
+          url: shareUrl,
+        });
+      } else {
+        await navigator.clipboard.writeText(shareUrl);
+        showToast("링크가 복사 되었어요.", "notify");
+      }
+    } catch (error) {
+      showToast("취소 했어요.");
+    } finally {
+      hideProgress();
+    }
+  };
+
   return (
     <Container>
       {playList?.map((item) => (
-        <Item
-          key={item.sheetName}
-          to={`/quiz?id=${getSheetId(item.sheetId)}&name=${
-            item.sheetName
-          }&stage=${totalStage}`}
-        >
+        <Item key={item.sheetName}>
           <PlayBtn>
-            <DateSection>
+            <ShareSection>
               <span className="tag">최근 점수</span>
-              <span className="date">{renderDate(item.sheetName)}</span>
-            </DateSection>
+              <ShareButton
+                onClick={() => shareItem(item.sheetId, item.sheetName)}
+              >
+                <Share />
+              </ShareButton>
+            </ShareSection>
             <ScoreSection>{renderScore(item.sheetName)}</ScoreSection>
-            <LabelSection>
+            <LabelSection
+              onClick={() => goToQuizPage(item.sheetId, item.sheetName)}
+            >
               <span className="label">{item.sheetName}</span>
               <Arrow fill={Color.Gray[100]} />
             </LabelSection>
@@ -80,7 +93,7 @@ function Actions({ data, unsigned }: Props) {
         </Item>
       ))}
       {unsigned || (
-        <Item to={"/setting"}>
+        <Item onClick={() => navigate("/setting")}>
           <AddSheetBtn>
             <img src={AddSheetImg} alt="시트 추가" />
             <span className="label">시트 추가</span>
@@ -101,13 +114,11 @@ const Container = styled.div`
   margin-bottom: 165px;
 `;
 
-const Item = styled(Link)`
+const Item = styled.div`
   position: relative;
   width: calc((100% - 16px) / 2);
   height: 0;
   padding-bottom: calc((100% - 16px) / 2);
-  cursor: pointer;
-  user-select: none;
 
   & .info {
     white-space: pre-line;
@@ -116,26 +127,29 @@ const Item = styled(Link)`
   }
 `;
 
-const PlayBtn = styled.div`
+const ItemLayout = css`
   position: absolute;
   top: 0;
   left: 0;
-  display: flex;
-  flex-direction: column;
   width: 100%;
   height: 100%;
-  background: ${Color.Primary[700]};
-  padding: 16px;
-  box-sizing: border-box;
-  color: ${Color.Gray[100]};
   border-radius: 24px;
   overflow: hidden;
 `;
 
-const DateSection = styled.div`
+const PlayBtn = styled.div`
+  ${ItemLayout};
+  display: flex;
+  flex-direction: column;
+  background: ${Color.Primary[700]};
+  color: ${Color.Gray[100]};
+`;
+
+const ShareSection = styled.div`
   display: flex;
   align-items: center;
-  gap: 5px;
+  justify-content: space-between;
+  padding: 16px 16px 0 16px;
   margin-bottom: 3.0303%;
 
   & .tag {
@@ -146,16 +160,28 @@ const DateSection = styled.div`
     border-radius: 16px;
     overflow: hidden;
   }
+`;
 
-  & .date {
-    color: ${Color.Primary[400]};
-    ${Body.SemiBold.S};
+const ShareButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  user-select: none;
+
+  & svg {
+    width: 16px;
+    height: 16px;
   }
 `;
 
 const ScoreSection = styled.div`
   display: flex;
   align-items: center;
+  gap: 2px;
+  padding-inline: 16px;
 
   & .score {
     font-family: "Black Ops One";
@@ -176,7 +202,13 @@ const LabelSection = styled.div`
   align-items: center;
   gap: 4px;
   width: 100%;
+  height: 28.0487%;
+  padding: 14px 16px;
+  box-sizing: border-box;
   margin-top: auto;
+  background: ${Color.Primary[600]};
+  cursor: pointer;
+  user-select: none;
 
   & .label {
     ${Body.Bold.M};
@@ -190,20 +222,16 @@ const LabelSection = styled.div`
 `;
 
 const AddSheetBtn = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
+  ${ItemLayout};
   border: 2px dashed ${Color.Gray[400]};
   box-sizing: border-box;
-  border-radius: 24px;
-  overflow: hidden;
   display: flex;
   gap: 10px;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
+  user-select: none;
 
   & img {
     width: 48px;
